4b74f30d6f5342851428fb4273d4db42e67b670a
evelan <evelan3094@gmail.com>
2015-12-13 01:34:17 +0100

    sprawozdanie

--- oswietlenie/oswietlenie/main.cpp
+++ oswietlenie/oswietlenie/main.cpp
@@ -315,76 +315,23 @@ void MyInit(void)
 {
 
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	// Kolor czyszcz¹cy (wype³nienia okna) ustawiono na czarny
-	/*************************************************************************************/
-
-	//  Definicja materia³u z jakiego zrobiony jest czajnik
-	//  i definicja Ÿród³a œwiat³a
-
-	/*************************************************************************************/
-
-
-	/*************************************************************************************/
-	// Definicja materia³u z jakiego zrobiony jest czajnik
 
 	GLfloat mat_ambient[] = { 1.0,1.0, 1.0, 1 };
-	// wspó³czynniki ka =[kar,kag,kab] dla œwiat³a otoczenia
-
 	GLfloat mat_diffuse[] = { 1.0, 1.0, 1.0, 1 };
-	// wspó³czynniki kd =[kdr,kdg,kdb] œwiat³a rozproszonego
-
 	GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
-	// wspó³czynniki ks =[ksr,ksg,ksb] dla œwiat³a odbitego               
-
 	GLfloat mat_shininess = { 20.0 };
-	// wspó³czynnik n opisuj¹cy po³ysk powierzchni
-
-	/*************************************************************************************/
-	// Definicja Ÿród³a œwiat³a
-
 	GLfloat light_position[] = { 0.0, 0.0, 30.0, 1.0 };
-	// po³o¿enie Ÿród³a
-
-
 	GLfloat light_ambient[] = { 1.0, 1.0, 0.0, 1.0 };
-	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a otoczenia
-	// Ia = [Iar,Iag,Iab]
-
 	GLfloat light_diffuse[] = { 1.0, 1.0, 0.0, 1.0 };
-	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a powoduj¹cego
-	// odbicie dyfuzyjne Id = [Idr,Idg,Idb]
-
 	GLfloat light_specular[] = { 1.0, 1.0, 0.0, 1.0 };
-	// sk³adowe intensywnoœci œwiecenia Ÿród³a œwiat³a powoduj¹cego
-	// odbicie kierunkowe Is = [Isr,Isg,Isb]
-
 	GLfloat att_constant = { 1.0 };
-	// sk³adowa sta³a ds dla modelu zmian oœwietlenia w funkcji
-	// odleg³oœci od Ÿród³a
-
 	GLfloat att_linear = { (GLfloat) 0.05 };
-	// sk³adowa liniowa dl dla modelu zmian oœwietlenia w funkcji
-	// odleg³oœci od Ÿród³a
-
 	GLfloat att_quadratic = { (GLfloat) 0.001 };
-	// sk³adowa kwadratowa dq dla modelu zmian oœwietlenia w funkcji
-	// odleg³oœci od Ÿród³a
-
-	/*************************************************************************************/
-
-	// Ustawienie parametrów materia³u i Ÿród³a œwiat³a
-
-	/*************************************************************************************/
-	// Ustawienie patrametrów materia³u
-
 
 	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
 	glMaterialfv(GL_FRONT, GL_AMBIENT, mat_ambient);
 	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
 	glMaterialf(GL_FRONT, GL_SHININESS, mat_shininess);
-
-	/*************************************************************************************/
-	// Ustawienie parametrów Ÿród³a
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
@@ -395,17 +342,10 @@ void MyInit(void)
 	glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, att_linear);
 	glLightf(GL_LIGHT0, GL_QUADRATIC_ATTENUATION, att_quadratic);
 
-
-	/*************************************************************************************/
-	// Ustawienie opcji systemu oœwietlania sceny
-
 	glShadeModel(GL_SMOOTH); // w³aczenie ³agodnego cieniowania
 	glEnable(GL_LIGHTING);   // w³aczenie systemu oœwietlenia sceny
 	glEnable(GL_LIGHT0);     // w³¹czenie Ÿród³a o numerze 0
 	glEnable(GL_DEPTH_TEST); // w³¹czenie mechanizmu z-bufora
-
-							 /*************************************************************************************/
-
 }
 
 /*************************************************************************************/
